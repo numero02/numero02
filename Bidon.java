5fa8cc2b24325e98db598ca1b34085ed42f05443
numero02 <32373494+numero02@users.noreply.github.com>
2017-09-28 16:21:05 -0400

    Add files via upload
    bidon

---
+++ Bidon.java
@@ -0,0 +1,81 @@
+package LindenMayer;
+
+import java.awt.geom.Point2D;
+
+public class Bidon implements Turtle {
+	
+	Point2D position;
+	double angle ;
+
+	public Bidon(){
+	
+		
+		
+	}
+
+	public void draw() {
+		
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnR() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnL() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stay() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(Point2D position, double angle_deg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Point2D getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getAngle() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setUnits(double step, double delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
